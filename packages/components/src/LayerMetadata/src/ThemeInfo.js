7f3a1287012be00a657d891aaa62c184e2152068
ricardogsena <ricardogsena@gmail.com>
2022-03-04 01:05:11 +0000

    Add config option to hide service metadata information

--- packages/components/src/LayerMetadata/src/ThemeInfo.js
+++ packages/components/src/LayerMetadata/src/ThemeInfo.js
@@ -31,58 +31,62 @@ class ThemeInfo extends React.Component {
         <div className="p-grid">
             <div className="p-col-4">Metadados</div>
             <div className="p-col-8"><a href={theme.metadata.url} target="_blank" style={{"textDecoration": "none"}}><i className="pi pi-external-link p-mr-2"></i>Abrir Ficha</a></div>
-        </div> : null }        
-        <div className="p-grid">
-            <div className="p-col-4">Tipo</div>
-            <div className="p-col-8">{this.getTypeDescription(theme.type)}</div>
-        </div>
-        { !['GROUP'].includes(theme.type) ? (
-        <div className="p-grid">
-            <div className="p-col-4">URL</div>
-            <div className="p-col-8" style={{wordBreak: "break-all"}}>{theme.url}</div>
-        </div>
-        ) : null }
-        { !['GROUP'].includes(theme.type) ? (
-        <div className="p-grid">
-            <div className="p-col-4">Sistema de Coordenadas</div>
-            <div className="p-col-8">
-                { theme.crs_options ? (
-                    <ul>
-                    { theme.crs_options.split(',').map((t, j) => (
-                        <li key={j}>{t}</li>
-                    ))}
-                    </ul>
-                ) : null }
+        </div> : null }
+        { (theme.exclude_service_metadata !== true) ?
+        (<>               
+            <div className="p-grid">
+                <div className="p-col-4">Tipo</div>
+                <div className="p-col-8">{this.getTypeDescription(theme.type)}</div>
             </div>
-        </div>
-        ) : null }
-        { ['WMS'].includes(theme.type) ? (
-        <div className="p-grid">
-            <div className="p-col-4">Formatos de GetMap</div>
-            <div className="p-col-8">
-                { theme.get_map_formats ? (
-                    <ul>
-                    { theme.get_map_formats.split(',').map((t, j) => (
-                        <li key={j}>{t}</li>
-                    ))}
-                    </ul>
-                ) : null }
+            { !['GROUP'].includes(theme.type) ? (
+            <div className="p-grid">
+                <div className="p-col-4">URL</div>
+                <div className="p-col-8" style={{wordBreak: "break-all"}}>{theme.url}</div>
             </div>
-        </div>
-        ) : null }
-        { ['WMS'].includes(theme.type) ? (
-        <div className="p-grid">
-            <div className="p-col-4">Formatos de GetFeatureInfo</div>
-            <div className="p-col-8">
-                { theme.get_feature_info_formats ? (
-                    <ul>
-                    { theme.get_feature_info_formats.split(',').map((t, j) => (
-                        <li key={j}>{t}</li>
-                    ))}
-                    </ul>
-                ) : null }
+            ) : null }
+            { !['GROUP'].includes(theme.type) ? (
+            <div className="p-grid">
+                <div className="p-col-4">Sistema de Coordenadas</div>
+                <div className="p-col-8">
+                    { theme.crs_options ? (
+                        <ul>
+                        { theme.crs_options.split(',').map((t, j) => (
+                            <li key={j}>{t}</li>
+                        ))}
+                        </ul>
+                    ) : null }
+                </div>
             </div>
-        </div>
+            ) : null }
+            { ['WMS'].includes(theme.type) ? (
+            <div className="p-grid">
+                <div className="p-col-4">Formatos de GetMap</div>
+                <div className="p-col-8">
+                    { theme.get_map_formats ? (
+                        <ul>
+                        { theme.get_map_formats.split(',').map((t, j) => (
+                            <li key={j}>{t}</li>
+                        ))}
+                        </ul>
+                    ) : null }
+                </div>
+            </div>
+            ) : null }
+            { ['WMS'].includes(theme.type) ? (
+            <div className="p-grid">
+                <div className="p-col-4">Formatos de GetFeatureInfo</div>
+                <div className="p-col-8">
+                    { theme.get_feature_info_formats ? (
+                        <ul>
+                        { theme.get_feature_info_formats.split(',').map((t, j) => (
+                            <li key={j}>{t}</li>
+                        ))}
+                        </ul>
+                    ) : null }
+                </div>
+            </div>
+            ) : null }
+        </>
         ) : null }
 
         { core.renderComponents({
