749a5a76112eadf866aeb3b3ad37a029c66f2930
ricardogsena <ricardogsena@gmail.com>
2023-08-27 11:47:37 +0100

    Show locale options inline or with a dropdown

--- packages/components/src/LocaleSelector/src/Main.js
+++ packages/components/src/LocaleSelector/src/Main.js
@@ -1,8 +1,11 @@
 import React, { useMemo } from 'react';
 import { useTranslation} from "react-i18next";
 import { Dropdown } from 'primereact/dropdown';
+import { Button } from 'primereact/button';
 
 import './style.css';
+
+const defaultMode = 'dropddown';
 
 /**
  * Main menu component
@@ -20,7 +23,7 @@ export default function Main({ as, config, actions, record }) {
   const locale = useMemo(()=> {
     return viewer.locale ? viewer.locale : i18n.language;
   }, [viewer.locale]);
-
+  
   const localesList = useMemo(() => {
     let locales = [];
     if (record?.config_json?.locales?.length) {
@@ -31,18 +34,51 @@ export default function Main({ as, config, actions, record }) {
       }
     } else {
       locales = [
-        { label: i18n.language, value: i18n.language}
+        { label: i18n.language.toUpperCase(), value: i18n.language}
       ]
     }
     return locales;
-  }, [record?.config_json, locale]);
+  }, [record?.config_json, locale]);  
+
+  const mode = useMemo(() => {
+    if (!record?.config_json?.mode && localesList?.length <= 3) {
+      return 'inline';
+    }
+    return (record?.config_json?.mode || defaultMode).toLowerCase();
+  });  
 
   const handleChange = (event) => {
     dispatch(viewer_set_locale(event.value));
   }
 
-  return (
-    <Dropdown value={locale.toLowerCase()} options={localesList} onChange={handleChange} className='locale-selector p-ml-3 p-ml-3' />
-  );
 
+  if (localesList.length < 2 && !(record?.config_json?.show_always === true)) {
+    return null;
+  } 
+  
+  if (mode === 'inline') {
+    return (
+      <div style={{marginLeft: '20px'}} className='locale-selector'>
+        {localesList.map((item) => {
+          return (
+            <Button
+              key={item.value} 
+              label={item.label} 
+              style={{
+                marginLeft: 'unset',
+                minWidth: 'unset',
+              }}
+              className={`p-button-text ${ i18n.language === item.value ? 'locale-selected' : ''}`}
+              onClick={(e)=>dispatch(viewer_set_locale(item.value))}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+
+  return (
+    <Dropdown value={locale.toLowerCase()} options={localesList} onChange={handleChange} className='locale-selector' />
+  );  
+  
 }
