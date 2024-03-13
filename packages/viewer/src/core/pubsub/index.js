ef2745422052d65e0498f6becfa83d58802906a3
Ricardo Sena <ricardo.sena@wkt.pt>
2024-03-13 14:28:50 +0000

    Refactor pubsub module: avoid duplicated subscriptions, add option to override existing subsctiption

--- packages/viewer/src/core/pubsub/index.js
+++ packages/viewer/src/core/pubsub/index.js
@@ -1,19 +1,30 @@
-import { v4 as uuid } from 'uuid'
-const topics = {}
+import { v4 as uuid } from 'uuid';
+const topics = {};
 
-export function subscribe(topic, fn) {
-  if (!topics[topic]) topics[topic] = {}
-  const id = uuid()
-  topics[topic][id] = fn
+export function subscribe(topic, fn, override=true) {
+  let id = uuid();
+
+  if (!topics[topic]) {
+    topics[topic] = fn;
+  } else {
+    if (override) {
+      topics[topic] = null;
+      delete topics[topic];
+
+      topics[topic] = fn;
+    }
+  }
+
   return () => {
-    topics[topic][id] = null
-    delete topics[topic][id]
+    if (!topics[topic]) {
+      topics[topic] = null;
+      delete topics[topic];
+    }
   }
 }
 
 export function publish(topic, args) {
-  if (!topics[topic]) return
-  Object.values(topics[topic]).forEach(fn => {
-    if (fn) fn(args)
-  })
+  if (!topics[topic]) return;
+  const fn = topics[topic];
+  if (fn) fn(args);
 }
