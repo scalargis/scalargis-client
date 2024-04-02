c474336d414dad9551cbe34ac16e706fce7b77ef
Ricardo Sena <ricardo.sena@wkt.pt>
2024-04-02 15:56:10 +0100

    PopupInfo: add french translation

--- packages/components/src/PopupInfo/src/i18n/index.js
+++ packages/components/src/PopupInfo/src/i18n/index.js
@@ -3,10 +3,13 @@ import { loadTranslations as _loadTranslations } from "../../../utils/i18n";
 import pt from './translations/pt.json';
 import en from './translations/en.json';
 import es from './translations/es.json';
+import fr from './translations/fr.json';
 
 const translations = {
   pt,
   en,
+  es,
+  fr
 }
 
 export const I18N_NAMESPACE = "PopupInfo";
