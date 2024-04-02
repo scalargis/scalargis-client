abd45ddaa5c08f1bb234f812b4c537b52a427f6b
Ricardo Sena <ricardo.sena@wkt.pt>
2024-04-02 18:52:38 +0100

    Add french translation

--- packages/viewer/src/core/i18n/translations/index.js
+++ packages/viewer/src/core/i18n/translations/index.js
@@ -1,11 +1,13 @@
 import pt from './pt.json';
 import en from './en.json';
 import es from './es.json';
+import fr from './fr.json';
 
 const translations = {
   pt,
   en,
-  es
+  es,
+  fr
 }
 
 export default translations;
